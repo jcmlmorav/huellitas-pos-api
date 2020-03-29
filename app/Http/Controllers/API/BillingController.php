bfd776105469d1c09871a7fd05149ebe73a1796f
Juan Camilo Mora <jcmlmorav@gmail.com>
2020-03-28 19:25:37 -0500

    Add capability to update billing

--- app/Http/Controllers/API/BillingController.php
+++ app/Http/Controllers/API/BillingController.php
@@ -103,7 +103,65 @@ class BillingController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $billing = Billing::find($id);
+
+        if ($billing) {
+            $validator = Validator::make($request->all(), [
+                'total' => 'required|numeric',
+                'products_quantity' => 'required|integer',
+                'products' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return ['error' => $validator->errors()];
+            } else {
+                $billing->client_document = $request->client_document;
+                $billing->total = $request->total;
+                $billing->products_quantity = $request->products_quantity;
+                $billing->coupon = $request->coupon;
+                $billing->coupon_discount = $request->coupon_discount;
+                $billing->money = $request->money;
+                $billing->change = $request->change;
+
+                $billing->save();
+
+                foreach ($billing->products as $product) {
+                    $query = Product::where('barcode', $product['barcode'])->first();
+                    $selected_product = Product::find($query->id);
+                    if($selected_product) {
+                        $selected_product->quantity = $selected_product->quantity + $product['quantity'];
+                        $selected_product->save();
+                    }
+                }
+
+                $billing->products()->detach();
+
+                foreach ($request->products as $product) {
+                    $query = Product::where('barcode', $product['barcode'])->first();
+    
+                    $product_billing = ProductBilling::create([
+                        'product_id' => $query->id,
+                        'billing_id' => $billing->id,
+                        'quantity' => $product['quantity'],
+                        'price' => $product['price'],
+                        'discount' => $product['discount'],
+                        'total' => ($product['quantity'] * $product['price']) * ((100 - $product['discount']) / 100)
+                    ]);
+    
+                    $selected_product = Product::find($query->id);
+                    if($selected_product) {
+                        $selected_product->quantity = $selected_product->quantity - $product['quantity'];
+                        $selected_product->save();
+                    }
+                }
+
+                return ['data' => $billing];
+            }
+        } else {
+            return ['error' => [
+                'billing' => ['La factura que intenta actualizar no existe']
+            ]];
+        }
     }
 
     /**
